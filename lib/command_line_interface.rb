19d8f2c0fb7ada37055eba2f10975c6651583e5b
mkenane <morkenan@hotmail.com>
2018-03-13 15:59:23 -0400

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,16 @@
 def welcome
-  # puts out a welcome message here!
+  "Welcome to Star Wars Central"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "Please enter a character"
+  character = gets.chomp
 end
+
+
+
+#git status
+#git add .
+#git status
+#git commit -m "done"
+#git push
